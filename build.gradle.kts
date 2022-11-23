97f6308223141bc3deb03629361dd5e4d4801f8f
Msksgm <msksgm3124@gmail.com>
2022-11-24 08:35:41 +0900

    chore: ArchUnit を package に追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,26 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	/**
+	 * Archunit
+	 *
+	 * MavenCentral
+	 * - https://mvnrepository.com/artifact/com.tngtech.archunit/archunit-junit5
+	 * Main 用途
+	 * - アーキテクチャの設計思想を単体テスト化
+	 * Sub 用途
+	 * - なし
+	 * 概要
+	 * - ArchUnit の JUnit5 バージョン
+	 *   - 他には、JUnit4 バージョンやプレーンな archunit が存在する。
+	 *   - 本リポジトリでは JUnit5 バージョン以外不要
+	 * - アーキテクチャの設計思想を守れているか、単体テストで確認可能になる
+	 *   - CI に組み込むことで、PR、マージ時点で、発見可能
+	 * - テスト対象
+	 *   - パッケージ、クラス、レイヤー、循環参照など
+	 */
+	testImplementation("com.tngtech.archunit:archunit-junit5:1.0.0")
 }
 
 tasks.withType<KotlinCompile> {
