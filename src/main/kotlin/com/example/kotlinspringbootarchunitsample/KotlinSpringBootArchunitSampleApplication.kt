74cb6d2a4df72f12b5de95d60b72554e59156dcd
Msksgm <msksgm3124@gmail.com>
2022-11-24 08:22:20 +0900

    first commit

---
+++ src/main/kotlin/com/example/kotlinspringbootarchunitsample/KotlinSpringBootArchunitSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinspringbootarchunitsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootArchunitSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringBootArchunitSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinspringbootarchunitsample/KotlinSpringBootArchunitSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringbootarchunitsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootArchunitSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
